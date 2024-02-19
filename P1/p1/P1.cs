cb4f9e3ee112e79fcb1842832e6b5363788f1f0d
Zhou Liu <111816533+zliu46@users.noreply.github.com>
2024-02-19 08:04:19 -0800

    Add files via upload

---
+++ P1/p1/P1.cs
@@ -0,0 +1,172 @@
+﻿//Zhou Liu
+//P1.cs
+//This is the program that tests each function of jumpPrime class.
+//1/19/23
+//@verstion 1.0
+
+using System;
+
+namespace p1
+{
+    class p1
+    {
+        //This is the test method to test creating a new jumpPrime object.
+        //I hardcode the number for each function because it would be easier
+        //for me to test different numbers.
+        public static void Test2488()
+        {
+            //Create a new jumpPrime Object.
+            jumpPrime myObj = new jumpPrime(2488);
+
+        }
+
+        //This method tests creating a jumpPrime object by using a negative
+        //number.
+        public static void TestNegativeInput()
+        {
+            //Passing -1 to jumpPrime class.
+            //It will throw an exception showing "invalid number".
+            jumpPrime myObj = new jumpPrime(-1);
+        }
+
+        //This method tests the Up function of jumpPrime class.
+        public static void TestUp()
+        {
+            //Create a new obj.
+            jumpPrime myObj = new jumpPrime(2488);
+            //Call a Up function.
+            //Prints out the next prime number.
+            Console.WriteLine("Nearest prime more than given number: " +
+                myObj.up() + "\n");
+
+        }
+
+        //This method tests the Down function of jumpPrime class.
+        public static void TestDown()
+        {
+            //Create a new obj.
+            jumpPrime myObj = new jumpPrime(2488);
+
+            //Call a Down function.
+            //Prints out the next prime number.
+            Console.WriteLine("Nearest prime less than given number: " +
+                        myObj.down() + "\n");
+
+        }
+
+        //This is the method that tests the Reset function.
+        public static void TestReset()
+        {
+            //Create a new object.
+            jumpPrime myObj = new jumpPrime(2488);
+            //Call a reset function.
+            myObj.reset();
+            //Prints out the number after reset.
+            Console.WriteLine(myObj.getNum());
+        }
+
+        //This method tests the Revive function.
+        public static void TestRevive()
+        {
+            //Create a new Object.
+            jumpPrime myObj = new jumpPrime(2488);
+            //Call a revive function..
+            myObj.revive();
+            //Call a getStatus function to prints out the Status.
+            myObj.getStatus();
+        }
+
+        //This method tests the GetStatus function.
+        public static void TestGetStatus()
+        {
+            //Create a new Obj.
+            jumpPrime myObj = new jumpPrime(2488);
+            //Call a getStatus function.
+            myObj.getStatus();
+        }
+
+        //This method tests the getCurrNum function.
+        public static void TestGetCurrNum()
+        {
+            //Create a new object.
+            jumpPrime myObj = new jumpPrime(2488);
+            //Call a getNum function and prints it out.
+
+            Console.WriteLine(myObj.getNum());
+        }
+
+        //This method tests the getQuery function.
+        public static void TestGetQuery()
+        {
+            //Create a new Object.
+            jumpPrime myObj = new jumpPrime(2488);
+            //Jump down the number.
+            myObj.down();
+            //Prints out the query count.
+            Console.WriteLine(myObj.getQuery());
+        }
+        //This method tests ranOutOfRangeLow function.
+        public static void TestRanOutOfRangeLow()
+        {
+            //Create a new Object.
+            jumpPrime myObj = new jumpPrime(1200);
+
+            //Jump down the number until it is out of range.
+            for (int i = 0; i < 100; i++)
+            {
+                myObj.down();
+            }
+        }
+        //This method tests ranOutOfRangeHigh function.
+        public static void TestRanOutOfRangeHigh()
+        {
+            //Create a new object.
+            jumpPrime myObj = new jumpPrime(9500);
+
+            //Jump up the number until it is out of range.
+            for (int i = 0; i < 100; i++)
+            {
+                myObj.up();
+            }
+        }
+
+        //This is the main method that runs each test method.
+        public static void Main(string[] args)
+        {
+            //Call a Test2488 method.
+            Test2488();
+
+            //call a TestUp method.
+            TestUp();
+
+            //call a TestDown method.
+            TestDown();
+
+            //Call a TestNegativeInput method.
+            //TestNegativeInput();
+
+            //Call a TestReset method.
+            TestReset();
+
+            //Call a Revive method.
+            TestRevive();
+
+            //Call a TestRanOutOfRangeHigh method.
+            TestRanOutOfRangeHigh();
+
+            //Call a TestRanOutOfRangeLow method.
+            TestRanOutOfRangeLow();
+
+            //Call a TestGetQuery method.
+            TestGetQuery();
+
+            //Call a TestGetStatus method.
+            TestGetStatus();
+
+            //Call a TestGetCurrNum method.
+            TestGetCurrNum();
+        }
+
+        
+    }
+}
---
+++ P1/p1/jumpPrime.cs
@@ -0,0 +1,197 @@
+﻿//Zhou Liu
+//jumpPrime.cs
+//This is the jumpPrime class.
+//1/19/23
+//@verstion 1.0
+
+using System;
+namespace p1
+{
+	public class jumpPrime
+	//This class takes a 4 digits number.
+	//This class allows the user to jump the input number to the next
+	//prime number.
+	//The user feels free to keep jumpping the number until the current number
+	//has become out of range.
+	//This class allows the user to reset the object to the original version.
+	//This class allows the user to acheive stats such as status, query count,
+	//and current number.
+    {
+		private int number;//To hold the orignial number.
+
+		//Create a enum to store different status.
+		private enum Status { Active, Inactive, Deactivated };
+
+		//To hold the status.
+		private Status status;
+
+		//To count the queries.
+		private int queryCount;
+
+		//To hold the current number after jump.
+		private int currNum;
+
+		//argument constructor
+		//@param: input number from the user.
+		public jumpPrime(int num)
+		{
+			//Check if the input number is in range.
+			//Throws an exception if the number is invalid.
+			if (num > 9999 || num < 1000)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			
+			number = num;//Initialize the original number.
+			currNum = num;//Initialize the current number.
+			status = Status.Active;//Set status.
+			queryCount = 0;//Initialize the query count.
+		}
+
+		//This function checks if the number is a prime number.
+		//@param: an integer number.
+		//@return: boolean if the number is a prime number.
+		private static bool isPrime(int curr)
+		{
+			//Call a for loop to check if it is a prime number.
+			for (int i = 2; i <= Math.Sqrt(curr); i++)
+			{
+				if (curr % i == 0)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		//This function checks if the current number is out of range.
+		//@return: boolean if the current number is invalid.
+		private bool outOfRange()
+		{
+			//Check if the current number is not a 4 digits number.
+			if (currNum < 1000 || currNum > 9999)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		//This is the up function that jump up the number to the next
+		//prime number
+		//@return: the next prime number.
+		public int up()
+		{	//Check if the current number is valid.
+			//Throw an exception if the number is out of range.
+			if (outOfRange())
+			{
+				throw new Exception("out of range");
+			}
+			else
+			{
+				//Check the next number until it is a prime number.
+				int temp = currNum + 1;
+				while (!isPrime(temp))
+				{
+					temp++;
+				}
+				//Computing the queries that took to jump.
+				//Set queryCount.
+				queryCount += temp - number;
+				//Set currNum to the next prime number.
+				currNum = temp;
+				//Return the prime number.
+				return temp;
+			}
+		}
+
+        //This is the up function that jump down the number to the next
+        //prime number.
+		//@return: the next prime number.
+        public int down()
+        {
+			// Check if the current number is valid.
+            //Throw an exception if the number is out of range.
+            if (outOfRange())
+			{
+				throw new Exception("out of range");
+			}
+			else
+			{
+				int temp = currNum - 1;
+                //Call a while loop to check the next number until
+				//it is a prime number.
+                while (!isPrime(temp))
+				{
+					temp--;
+				}
+                //Computing the queries that took to jump.
+                //Set queryCount.
+                queryCount += number - temp;
+                //Set currNum to the next prime number.
+                currNum = temp;
+                //Return the prime number.
+                return temp;
+			}
+        }
+		//This fuction revives the status of jumpPrime object.
+		public void revive()
+		{
+            //Set object to be permanently deactivated when it's already active.
+            if (status == Status.Active)
+			{
+				status = Status.Deactivated;
+			}
+			//Set object to be active if it was inactive.
+			else if (status == Status.Inactive)
+			{
+				status = Status.Active;
+			}
+			//stay deactivated.
+			else
+				return;
+		}
+
+		//This function resets the object.
+		public void reset()
+		{
+			//Resets the status to be active.
+			status = Status.Active;
+			//Resets the query count.
+			queryCount = 0;
+			//Resets the current number to the original number.
+			currNum = number;
+		}
+
+        //accessor
+		//This function gets the current number.
+		//@return: the value of current number.
+        public int getNum() { return currNum; }
+
+		//This function gets the query count.
+		//@return: query count.
+		public int getQuery() { return queryCount; }
+
+		//This functions will prints out the current status of object.
+		public void getStatus()
+		{
+			//Call a switch statement to prints out the status of object.
+			switch (status)
+			{
+				case Status.Active:
+				
+					Console.WriteLine ("Active");
+					break;
+                case Status.Inactive:
+
+                    Console.WriteLine("Inactive");
+                    break;
+				case Status.Deactivated:
+                    Console.WriteLine("Deactivated");
+                    break;
+            }
+        }
+
+    }
+}
+
